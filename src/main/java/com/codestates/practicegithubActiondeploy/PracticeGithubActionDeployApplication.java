402afc7d2038ce34b04fe824e3a4fabffc05bbe3
RYEOL-KIM <rlaemrfuf043@gmail.com>
2023-06-01 16:24:02 +0900

    test 2dn

--- src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
@@ -2,11 +2,14 @@ package com.codestates.practicegithubActiondeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.codestates.practicegithubActiondeploy.controller")
 public class PracticeGithubActionDeployApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PracticeGithubActionDeployApplication.class, args);
 	}
 
